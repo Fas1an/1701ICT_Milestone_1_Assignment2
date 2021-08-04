0f1a9b9ee2aeb5b0a9715f77c903487de23c826d
Fas1an <47825077+Fas1an@users.noreply.github.com>
2021-08-04 11:58:52 +1000

    COMPLETED

--- Project 4/sketch.js
+++ Project 4/sketch.js
@@ -1,14 +1,15 @@
 var ang = 0
-var r = 0
+var r = 1
 var s = 1
-
+var cr1 = 75
+var cr2 = -75
 
 function setup() {
     // put setup code here
     createCanvas(500, 500);
     background(169, 169, 169);
     angleMode(DEGREES);
-    noStroke()
+    noStroke();
 }
 
 function draw() {
@@ -16,37 +17,29 @@ function draw() {
     fill(255)
     ellipseMode(CENTER);
     translate(250, 250);
-    arc(0, 0, 300, 300, PI, PI);
-    arc(0, 0, 280, 280, 180 + r, 360 + r)
-        //150lack semi-circle
+    arc(0, 0, 300, 300, 180 + r, 360 + r);
+    //150lack semi-circle
     fill(0);
     arc(0, 0, 300, 300, 0 + r, 180 + r);
 
-
-    var cx = 0 + cos(ang)
-    var cy = 0 + sin(ang)
-
-    var cx2 = 0 + cos(ang)
-    var cy2 = 0 + sin(ang)
-
     //150lack semi-circle 2
-    ellipseMode(CENTER)
+    var cx = cr2 * cos(ang);
+    var cy = cr2 * sin(ang);
     fill(0);
-    arc(cx2, cy2, 150, 150, 180, 180);
-
-    //white semi-circle 2
-    ellipseMode(CENTER)
-    fill(255);
-    arc(cx, cy, 150, 150, 180, 180);
-
+    ellipse(cx, cy, 150, 150);
     //small white circle
-
-    fill(0);
+    fill(255);
     ellipse(cx, cy, 50, 50);
 
-    //small white circle
+    var cx2 = cr1 * cos(ang);
+    var cy2 = cr1 * sin(ang);
+    //white semi-circle 2
     fill(255);
+    ellipse(cx2, cy2, 150, 150);
+    //small white circle
+    fill(0);
     ellipse(cx2, cy2, 50, 50);
-    ang = ang + 1
 
+    ang += s;
+    r++;
 }
